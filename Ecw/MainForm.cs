32f99869c4b43054fb260cfe66231ba25df86952
ScottZhou <ScottZhou@thinkgeo.com>
2019-01-08 13:31:52 +0800

    Update and test with the latest NuGet packages

--- Ecw/MainForm.cs
+++ Ecw/MainForm.cs
@@ -6,32 +6,32 @@ using System.IO;
 
 namespace ThinkGeo.MapSuite.Samples
 {
-    public partial class MainForm : Form
-    {
-        public MainForm()
-        {
-            InitializeComponent();
-        }
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
 
-        private void MainForm_Load(object sender, EventArgs e)
-        {
-            winformsMap1.MapUnit = GeographyUnit.DecimalDegree;
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			winformsMap1.MapUnit = GeographyUnit.DecimalDegree;
 
-            LayerOverlay overlay = new LayerOverlay();
+			LayerOverlay overlay = new LayerOverlay();
 
 			//To resolve issue that we cannot run the executable by double click it on linux, we need to find out the absolute path by reflection.
 			string baseDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
 			string ecwLayerFileName = Path.GetFullPath (Path.Combine (baseDirectory, "../../App_Data/World.ecw"));
 
 			EcwRasterLayer ecwRasterLayer = new EcwRasterLayer(ecwLayerFileName);
-            overlay.Layers.Add(ecwRasterLayer);
+			overlay.Layers.Add(ecwRasterLayer);
 
-            winformsMap1.Overlays.Add(overlay);
+			winformsMap1.Overlays.Add(overlay);
 
-            ecwRasterLayer.Open();
-            winformsMap1.CurrentExtent = ecwRasterLayer.GetBoundingBox();
+			ecwRasterLayer.Open();
+			winformsMap1.CurrentExtent = ecwRasterLayer.GetBoundingBox();
 
-            winformsMap1.Refresh();
-        }
-    }
+			winformsMap1.Refresh();
+		}
+	}
 }
